361af5bd0dbf7ec4642d6d1cb949a7d1eba5ca66
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-09-29 01:07:04 -0500

    ejercicio 1.4

--- part1/src/Total.js
+++ part1/src/Total.js
@@ -1,9 +1,14 @@
 import { React } from 'react'
 
-const Total = ({ total }) => (
-    <p>
-        Number of exercises {total}
-    </p>
-)
+const Total = ({ parts }) => {
+    const initialReduce = 0;
+    const total = parts.reduce(((suma, part) => suma + part.exercises), initialReduce)
+
+    return(
+        <p>
+            Number of exercises {total}
+        </p>
+    )
+}
 
 export default Total
