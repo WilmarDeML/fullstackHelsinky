52a7347d9cda9ddc00902aafd5cfee090255b8c8
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-10-05 01:30:31 -0500

    update file and exercise 1.14* ok

--- part1/src/anecdotes/index.js
+++ part1/src/anecdotes/index.js
@@ -11,7 +11,6 @@ const Anecdotes = () => {
     // Retorna un número aleatorio entre 0 (incluido) y length de anecdotes (excluido)
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-
   const handleOnClick = () => {
     setVotes(votes + 1)
     anecdotes[selected].votes += 1
@@ -19,10 +18,16 @@ const Anecdotes = () => {
 
   return (
     <>
+    <h1>Anecdote of the day</h1>
     <strong><Display text={anecdotes[selected].text} /></strong>
     <strong><Display text={`Has ${anecdotes[selected].votes} votes`} /></strong>
     <Button onClick={handleOnClick} text='Vote' />
     <Button onClick={handleClick} text='Next anecdote' />
+
+    <h1>Anecdote with most Votes</h1>
+    <strong><Display 
+      text={anecdotes.reduce((acc, act) => acc.votes > act.votes ? acc : act).text} 
+    /></strong>
     </>
   )
 }
