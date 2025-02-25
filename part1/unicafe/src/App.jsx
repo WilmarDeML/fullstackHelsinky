b8b19ea46339a9f6085f0c775febddeadac2b41b
WilmarDeML <carewaz23@hotmail.com>
2025-02-24 21:35:03 -0500

    solve exercise 1-11*

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,10 @@ const Button = ({text, onClick}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <>
-      <p>{text} {value}</p>
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -18,14 +19,16 @@ const Statistics = ({good, neutral, bad}) => {
   const total = good+neutral+bad
   if (total > 0) {
     return (
-      <>
-        <StatisticLine text='Good' value={good} />
-        <StatisticLine text='Neutral' value={neutral} />
-        <StatisticLine text='Bad' value={bad} />
-        <StatisticLine text='All' value={total} />
-        <StatisticLine text='Average' value={(good-bad) / total} />
-        <StatisticLine text='Positive' value={(good * 100) / total} />
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text='Good' value={good} />
+          <StatisticLine text='Neutral' value={neutral} />
+          <StatisticLine text='Bad' value={bad} />
+          <StatisticLine text='All' value={total} />
+          <StatisticLine text='Average' value={(good-bad) / total} />
+          <StatisticLine text='Positive' value={(good * 100) / total} />
+        </tbody>
+      </table>
     )    
   }
 
