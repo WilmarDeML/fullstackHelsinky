e7598fb4eba9bad258990f9b0b2c24123e98f566
WilmarDeML <carewaz23@hotmail.com>
2025-02-25 21:18:26 -0500

    solve exercise 2.9*

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -33,17 +37,27 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
-        <div>number: <input type='text' value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+        <div>
+          number: <input type='text' value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {!filter ? persons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      )) : persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase())).map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
