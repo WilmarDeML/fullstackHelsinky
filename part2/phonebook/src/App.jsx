c6a8a2010c207a7284c2bd58db796222704b4a26
WilmarDeML <carewaz23@hotmail.com>
2025-02-27 08:39:54 -0500

    solve exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, changeFilter, text }) => (
   <>
@@ -40,12 +41,7 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -76,6 +72,12 @@ const App = () => {
   const changeFilter = e => setFilter(e.target.value)
   const changeName = e => setNewName(e.target.value)
   const changeNumber = e => setNewNumber(e.target.value)
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(res => res.data)
+      .then(setPersons)
+  }, [])
 
   return (
     <>
