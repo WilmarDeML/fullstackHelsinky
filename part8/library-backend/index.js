3255af8d08e29ddc911b66a01c0692dab008cf61
WilmarDeML <carewaz23@hotmail.com>
2025-03-28 18:47:02 -0500

    solve exercise 8.16

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,9 +1,11 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
-import { PORT, MONGODB_URI } from './config.js'
+import { PORT, MONGODB_URI, SECRET } from './config.js'
 import Author from './models/author.js'
 import Book from './models/book.js'
+import User from './models/user.js'
 import { GraphQLError } from 'graphql'
 
 mongoose.set('strictQuery', false)
@@ -34,6 +36,7 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
   type Mutation {
     addBook(
@@ -42,9 +45,23 @@ const typeDefs = `
       author: String!, 
       genres: [String!]!
     ): Book      
-  }
-  type Mutation {
     editAuthor(name: String!, setBornTo: Int): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+  }
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  type Token {
+    value: String!
   }
 `
 
@@ -52,7 +69,14 @@ const filterBooks = async (_root, args) => {
   return await Book.find(args.genre ? { genres: args.genre } : {})
 }
 
-const addBook = async (_root, args) => {
+const addBook = async (_root, args, { currentUser }) => {
+  if (!currentUser) {
+    throw new GraphQLError('not authenticated', {
+      extensions: {
+        code: 'BAD_USER_INPUT'
+      }
+    })
+  }
   let author = await Author.findOne({ name: args.author })
   if (!author) {
     const newAuthor = new Author({ name: args.author })
@@ -81,7 +105,14 @@ const addBook = async (_root, args) => {
   return await newBook.populate('author')
 }
 
-const editAuthor = async (_root, args) => {
+const editAuthor = async (_root, args, { currentUser }) => {
+  if (!currentUser) {
+    throw new GraphQLError('not authenticated', {
+      extensions: {
+        code: 'BAD_USER_INPUT'
+      }
+    })
+  }
   let author = await Author.findOne({ name: args.name })
   if (author) {
     author.born = args.setBornTo
@@ -95,7 +126,8 @@ const resolvers = {
     bookCount: async () => await Book.countDocuments(),
     authorCount: async () => await Author.countDocuments(),
     allBooks: filterBooks,
-    allAuthors: async () => await Author.find()
+    allAuthors: async () => await Author.find(),
+    me: (_root, _args, { currentUser }) => currentUser
   },
   Author: {
     bookCount: async (author) =>
@@ -103,7 +135,35 @@ const resolvers = {
   },
   Mutation: {
     addBook,
-    editAuthor
+    editAuthor,
+    createUser: async (_root, args) => {
+      const user = new User({ username: args.username })
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating the user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      })
+    },
+    login: async (_root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const { username, id } = user
+
+      return { value: jwt.sign({ username, id }, SECRET) }
+    }
   }
 }
 
@@ -113,7 +173,15 @@ const server = new ApolloServer({
 })
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: PORT }
+  listen: { port: PORT },
+  context: async ({ req }) => {
+    const auth = req?.headers?.authorization
+    if (auth?.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(auth.split(' ')[1], SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 console.log(`🚀  Server ready at: ${url}`)
